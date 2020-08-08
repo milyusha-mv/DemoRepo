ab453dc274f33a3564eaafc44bbae2479c3e26fd
Mikhail M <milyusha-mv@ural.rt.ru>
2020-08-08 23:45:39 +0500

    Initial Commit

---
+++ DemoProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by Михаил Милюша on 08.08.2020.
+//  Copyright © 2020 Михаил Милюша. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
